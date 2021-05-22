e91adaf79a30073bb319bed9831f5be61f9f32ab
Pavel Zhachko <rammiel@yandex.ru>
2021-05-22 22:41:19 +0300

    feat: add webpack + typescript + eslint + prettier to project

---
+++ match-match-game/webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    hot: true,
+    port: 8080,
+    contentBase: path.join(__dirname, 'public'),
+    watchOptions: {
+      poll: true
+    }
+  }
+};
+
+const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] }) ]
+
+module.exports = ({ develop }) => ({
+  mode: develop ? 'development' : 'development',
+  devtool: develop? 'inline-source-map' : false,
+  entry: {
+    app: './src/index.ts'
+  },
+  // devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[name][ext]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // title: 'Demo webpack'
+      template: './src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './public'},
+        { from: path.resolve(__dirname, "./src/assets/images"), to: 'assets/images'},
+        { from: path.resolve(__dirname, "./src/assets/fonts"), to: 'assets/fonts'}
+      ]
+    }),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    ...esLintPlugin(develop)
+  ],
+  ...devServer(develop),
+});
